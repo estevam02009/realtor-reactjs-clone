26189f7c71e17987c54a55c72c227a48a355c2e1
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2024-07-07 23:12:50 -0300

    Criada página ForgotPassword para recuperação de senha

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,28 @@
 import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
+import { toast } from "react-toastify";
 import OAuth from "../components/OAuth";
 
 export default function ForgotPassword() {
+
+    const [email, setEmail] = useState("");
+
+    function onChange(e) {
+        setEmail(e.target.value);
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            await sendPasswordResetEmail(auth, email);
+            toast.success("Email de redefinição de senha enviado");
+        } catch (error) {
+            toast.error("Erro ao enviar o email de redefinição de senha");
+        }
+    }
+
     return (
         <section>
             <h1 className="text-3xl text-center font-bold mt-6">Recuperar Senha</h1>
@@ -16,10 +37,12 @@ export default function ForgotPassword() {
                 </div>
 
                 <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <input
                             type="email"
                             id="email"
+                            value={email}
+                            onChange={onChange}
                             placeholder="Email"
                             className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
                         />
